429dd978373e6e16bd7cb4616a00bdc38b3c9f51
Rahul R <100457954+rrahul08@users.noreply.github.com>
2023-02-26 21:31:40 +0530

    Add files via upload

---
+++ scrape.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+import pprint
+
+res = requests.get('https://news.ycombinator.com/')
+res2 = requests.get('https://news.ycombinator.com/?p=2')
+soup = BeautifulSoup(res.text, 'html.parser')
+soup2 = BeautifulSoup(res2.text, 'html.parser')
+links = soup.select('.titleline')
+subtext = soup.select('.subtext')
+
+links2 = soup2.select('.titleline')
+subtext2 = soup2.select('.subtext')
+
+mega_links = links + links2
+mega_subtext = subtext + subtext2
+
+def sort_by_votes(hnlist):
+   return sorted(hnlist, key= lambda k:k['vote'],reverse=True)
+
+def create_custom_hn(links, subtext):
+    hn = []
+    for idx,item in enumerate(links):
+        title = links[idx].getText()
+        href = links[idx].find('href',None)
+        vote = subtext[idx].select('.score')
+        if len(vote):
+         points = int(vote[0].getText().replace(' points', ''))
+         if points > 99:
+          hn.append({'title': title, 'vote' : points})
+    return sort_by_votes(hn)
+
+pprint.pprint(create_custom_hn( mega_links,mega_subtext))
